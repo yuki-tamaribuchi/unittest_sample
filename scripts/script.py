26614a82186b3cd1c1ec78f425eba27200339894
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-03-29 11:37:06 +0900

    ファイル内容実装

--- scripts/script.py
+++ scripts/script.py
@@ -1,6 +1,24 @@
 class calculate():
 
-    def __init__(self):
-        pass
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
 
-    
+
+    def add(self):
+        return self.x+self.y
+
+    def sub(self):
+        return self.x-self.y
+
+    def mul(self):
+        return self.x*self.y
+
+    def div(self):
+        return self.x//self.y
+
+    def mod(self):
+        return self.x%self.y
+
+    def double_plus_one(self):
+        return self.x*2+self.y*2+1
