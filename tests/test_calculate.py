109beb8c11b553d70a127599a5c7d84813c871ef
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-03-29 12:10:14 +0900

    テストコードの実装

---
+++ tests/test_calculate.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+from scripts.calculate import Caluculate
+
+
+class TestCalculate(TestCase):
+
+    def setUp(self):
+        self.c=Caluculate(10,2)
+
+    def test_add(self):
+        self.assertEqual(self.c.add(),12)
+
+    def test_sub(self):
+        self.assertEqual(self.c.sub(),8)
+
+    def test_mul(self):
+        self.assertEqual(self.c.mul(),20)
+
+    def test_div(self):
+        self.assertEqual(self.c.div(),5)
+
+    def test_mod(self):
+        self.assertEqual(self.c.mod(),0)
+
+    def test_double_plus_one(self):
+        self.assertEqual(self.c.double_plus_one(),25)
+
